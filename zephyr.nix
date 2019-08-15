219494070391f26a45c4bf3026b0bd0384f5ea9c
Ping Chen <remotenonsense@gmail.com>
2019-08-15 21:58:06 +0900

    Use forked zephyr

--- zephyr.nix
+++ zephyr.nix
@@ -3,11 +3,11 @@
 pkgs.stdenv.mkDerivation rec {
   name = "zephyr";
 
-  version = "v0.2.1";
+  version = "v0.3.0";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/coot/zephyr/releases/download/${version}/linux64.tar.gz";
-    sha256 = "0afcnpqabjs4b60grkcvz2hb3glpjhlnvqvpgc0zsdwaqnmcrrnk";
+    url = "https://github.com/rnons/zephyr/releases/download/${version}/linux64.tar.gz";
+    sha256 = "0p2xjnwjyllswagacrjfylfcpk8099rf16y5r2m9igfr6ch0pazm";
   };
 
   installPhase = ''
