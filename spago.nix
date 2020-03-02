4fc735c10a8eee4a5d044164621651b89d0d6782
Ping Chen <remotenonsense@gmail.com>
2020-03-02 17:19:15 +0900

    spago-0.14.0

--- spago.nix
+++ spago.nix
@@ -3,11 +3,11 @@
 pkgs.stdenv.mkDerivation rec {
   name = "spago";
 
-  version = "0.13.1";
+  version = "0.14.0";
 
   src = pkgs.fetchurl {
     url = "https://github.com/spacchetti/spago/releases/download/${version}/linux.tar.gz";
-    sha256 = "0s2ck0yd494mbjn21fghra2hh0h5mc5warpak19a8v7xbf65vhl5";
+    sha256 = "0bqpns70ik55wb5vahmrpaz480bm9nhq87iq57aj74w6v52qi3bv";
   };
 
   setSourceRoot = "sourceRoot=`pwd`";
